987352668845ac4c3748928df516e57aca79c916
mohamed <mohamed@me>
2023-09-18 04:40:00 +0300

    update

--- _printf.c
+++ _printf.c
@@ -10,9 +10,10 @@ int _printf(const char *format, ...)
 	int i, ch = 0, len = 0;
 	va_list args;
 
-	if (format == NULL)
-	return (-1);
+	
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] != '%')
@@ -24,7 +25,7 @@ int _printf(const char *format, ...)
 		{
 			i++;
 			if (format[i] == '\0')
-				break;
+				return (-1);
 			if (format[i] == '%')
 			{
 				write(1, &format[i], 1);
