ff73e59b754eab21bd6efacc1274fde05542f623
mohamed <mohamed@me>
2023-09-19 22:00:35 +0300

    fix

--- _printf.c
+++ _printf.c
@@ -18,6 +18,8 @@ int _printf(const char *format, ...)
 	va_list args;
 
 	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+	return (-1);
 	for (i = 0; format[i]; i++)
 	{
 		if (format[i] == '%')
